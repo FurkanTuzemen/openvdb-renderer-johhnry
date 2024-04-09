d9a66a41544f4bd059c9dbe0851f7cfd6a9b3d84
Furkan Tüzemen <furkantuzemen@gmail.com>
2024-04-09 14:53:09 +0300

    Add openvdb csg test code

--- src/vdb/VDBFile.cpp
+++ src/vdb/VDBFile.cpp
@@ -1,7 +1,21 @@
+#include "VDBFile.hpp"
+#include <openvdb/Grid.h>
+#include <openvdb/io/Stream.h>
 #include <openvdb/math/Coord.h>
 #include <openvdb/openvdb.h>
+#include <openvdb/tools/ChangeBackground.h>
+#include <openvdb/tools/Composite.h>
+#include <openvdb/tools/LevelSetSphere.h>
+#include <openvdb/tools/RayTracer.h>
+#include <openvdb/tools/VolumeToMesh.h>
 
-#include "VDBFile.hpp"
+using namespace openvdb::OPENVDB_VERSION_NAME::tools;
+
+#define FT_CHRONO_BEGIN std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+#define FT_CHRONO_END                                                                                                  \
+    std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();                                      \
+    std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count()    \
+              << "[ms]" << std::endl;
 
 #include <string>
 #include <vector>
@@ -13,6 +27,13 @@
 
 #include "geometry/CubeGeometry.hpp"
 #include "log/Log.hpp"
+
+openvdb::FloatGrid::Ptr createSphereGrid(float radius, openvdb::Vec3f center, float voxelSize) {
+    // Create a level set sphere grid
+    openvdb::FloatGrid::Ptr grid = openvdb::tools::createLevelSetSphere<openvdb::FloatGrid>(radius, center, voxelSize);
+
+    return grid;
+}
 
 std::string getGridType(openvdb::GridBase::Ptr grid) {
     if (grid->isType<openvdb::BoolGrid>())
@@ -107,12 +128,44 @@ void VDBFile::constructPointCloud(int gridIndex) {
     Log::success("Created " + std::to_string(nbPut) + " cubes!");
 }
 
+void VDBFile::constructPointCloud(const openvdb::FloatGrid *grid) {
+    // Remove previous cubes
+    cubes.clear();
+
+    int nbVoxels = 0;
+    int nbPut    = 0;
+    for (openvdb::FloatGrid::ValueOnCIter iter = grid->cbeginValueOn(); iter.test(); ++iter) {
+        if (iter.isVoxelValue() && nbVoxels % 100 == 0) {
+            openvdb::math::Coord pt = iter.getCoord();
+
+            IGeometry *cube = new CubeGeometry(pt.x() / 100.0, pt.y() / 100.0, pt.z() / 100.0, 1.0);
+            cubes.emplace_back(std::unique_ptr<IGeometry>(cube));
+
+            nbPut++;
+        }
+
+        nbVoxels++;
+    }
+
+    Log::success("Created " + std::to_string(nbPut) + " cubes!");
+}
+
 void VDBFile::draw(Program &displayProgram, float scale) {
     // Draw cubes
+    static auto sphereGrid1 = createSphereGrid(50.0, openvdb::Vec3f(0.0), 1.f);
+    static float i          = -50.f;
+    FT_CHRONO_BEGIN
+    auto sphereGrid2 = createSphereGrid(30.0, openvdb::Vec3f(0.0f + i, 30, 45), 1.f);
+    openvdb::tools::csgDifference(*sphereGrid1, *sphereGrid2);
+    i += .1f;
+
+    constructPointCloud(openvdb::gridPtrCast<openvdb::FloatGrid>(sphereGrid1).get());
     for (size_t i = 0; i < cubes.size(); i++) {
         cubes.at(i)->setScale(scale);
         cubes.at(i)->draw(displayProgram);
     }
+
+    FT_CHRONO_END
 }
 
 std::vector<std::string> &VDBFile::getGridNames() {
--- src/vdb/VDBFile.hpp
+++ src/vdb/VDBFile.hpp
@@ -12,28 +12,29 @@
 
 class VDBFile {
 public:
-  VDBFile(std::string filename);
-  virtual ~VDBFile() noexcept;
+    VDBFile(std::string filename);
+    virtual ~VDBFile() noexcept;
 
-  VDBFile(const VDBFile &other) = delete;
-  VDBFile(VDBFile &&other) noexcept = delete;
-  VDBFile &operator=(const VDBFile &other) = delete;
-  VDBFile &operator=(VDBFile &&other) noexcept = delete;
+    VDBFile(const VDBFile &other)                = delete;
+    VDBFile(VDBFile &&other) noexcept            = delete;
+    VDBFile &operator=(const VDBFile &other)     = delete;
+    VDBFile &operator=(VDBFile &&other) noexcept = delete;
 
-  std::vector<std::string> &getGridNames();
-  void displayMetaData(int gridIndex);
-  void draw(Program &displayProgram, float scale);
+    std::vector<std::string> &getGridNames();
+    void displayMetaData(int gridIndex);
+    void draw(Program &displayProgram, float scale);
 
-  void constructPointCloud(int gridIndex);
+    void constructPointCloud(int gridIndex);
+    void constructPointCloud(const openvdb::FloatGrid *grid);
 
-  std::string filename;
+    std::string filename;
 
 private:
-  openvdb::io::File *file;
-  std::vector<std::string> gridNames;
-  std::vector<openvdb::GridBase::Ptr> grids;
+    openvdb::io::File *file;
+    std::vector<std::string> gridNames;
+    std::vector<openvdb::GridBase::Ptr> grids;
 
-  std::vector<std::unique_ptr<IGeometry>> cubes;
+    std::vector<std::unique_ptr<IGeometry>> cubes;
 };
 
 #endif /* OPENVDB_RENDERER_SRC_VDB_FILE_HPP_ */
